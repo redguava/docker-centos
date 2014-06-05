2c96357195414fe0ad2b25a9706f9653df02e056
Matthew Jones <matt@geekle.id.au>
2014-06-05 15:35:27 +1000

    Remove fixed address and change to private address

--- environment.sh
+++ environment.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 for KEY in $(etcdctl --peers ${COREOS_PRIVATE_IPV4}:4001 ls --recursive /config | sed -e "s/\/config\///"); do
-  export ${KEY}=$(etcdctl --peers 172.17.8.101:4001 get /config/${KEY})
+  export ${KEY}=$(etcdctl --peers ${COREOS_PRIVATE_IPV4}:4001 get /config/${KEY})
 done
 
